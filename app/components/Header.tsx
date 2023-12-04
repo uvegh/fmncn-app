5c2444d890a9216dcb1315b7f1d87ee24cc642d6
vincent <uveghobamien@gmail.com>
2023-12-04 11:53:50 +0100

    ui changes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -59,7 +59,7 @@ function Header() {
               data-collapse-toggle="navbar-default"
               type="button"
               onClick={(e)=>{
-                e.stopPropagation()
+                // e.stopPropagation()
                 setIsOpen(!isOpen)
               }}
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -166,7 +166,7 @@ function Header() {
               onClick={() => {
                 setAppState({
                   ...appState,
-                  showModal: false,
+                  // showModal: false,
                   loginModal: true,
                 });
               }}
